313b6b63806a89428fc1ae48d8ba2e081b20e8e2
Mohammd Sharifi <mmdsharifi@yahoo.com>
2016-07-04 10:56:04 +0430

    Update : project structure - package.json

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ import '../lib/index.js';
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
+    http.get('http://127.0.0.1:3000/api', res => {
       assert.equal(200, res.statusCode);
       done();
     });
