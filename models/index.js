fa5c6c9efc826ea119cedff05f32f49ed58546d0
Mohammd Sharifi <mmdsharifi@yahoo.com>
2016-07-05 03:42:31 +0430

    chore(make restify work for all entities :+1:)

--- models/index.js
+++ models/index.js
@@ -1,11 +1,23 @@
 
+
 import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import passport from 'passport';
+import restify from 'express-restify-mongoose';
 import './database';
-import {UserController} from '../controllers/user';
 import authController from '../controllers/auth';
+
+import {UserController} from '../controllers/user';
+import Tag from './tag';
+import Role from './role';
+import Post from './post';
+import Page from './page';
+import Like from './like';
+import Event from './event';
+import Comment from './comment';
+import ChatRoom from './chatRoom';
+
 
 
 let userController= new UserController();
@@ -13,29 +25,36 @@ const app = express();
 const port = process.env.PORT || 3000;
 const router = express.Router();
 
-
-
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
 app.use(bodyParser.json());
 app.use(passport.initialize());
+
+var uri = restify.serve(router, Tag);
+console.log(uri);
+restify.serve(router, Role);
+restify.serve(router, Post);
+restify.serve(router, Page);
+restify.serve(router, Like);
+restify.serve(router, Event);
+restify.serve(router, Comment);
+restify.serve(router, ChatRoom);
 
 router.get('/', (req, res) => {
   res.json({ message: 'wellcome here !' });
 });
 
-router.route('/user')
+router.route('/api/user')
   .post(userController.postUsers)
   .get(userController.getUsers);
 
-router.route('/users/:user_id')
+router.route('/api/users/:user_id')
   .get(userController.getUser)
   .put(userController.putUser)
   .delete(userController.deleteUser)
 
-app.use('/api', router);
+app.use(router);
 app.listen(port);
 console.log(`Server Running on port :${port}`);
 
